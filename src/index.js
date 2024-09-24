f7de49c11dce43363245f2d162dda5a7bc06f122
gribVolnushka <burgerqueen98@gmail.com>
2024-09-24 11:35:24 +0200

    changed structure ternary to if-else

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const username = prompt('Enter login');
 const password = prompt('Enter password');
 
-username === 'admin' && password === 'qwerty1234'
+/*username === 'admin' && password === 'qwerty1234'
   ? alert('Hello, admin')
   : username === 'admin'
     ? alert('Incorrect admin password')
@@ -10,3 +10,19 @@ username === 'admin' && password === 'qwerty1234'
       : username === 'guest'
         ? alert('Incorrect guest password')
         : alert('Incorrect login data');
+
+ */
+
+if (username === 'admin' && password === 'qwerty1234') {
+  alert('Hello,admin');
+} else if (username === 'admin') {
+  alert('Incorrect admin password') ;
+} else if (username === 'guest' && password === 'asdfg1234') {
+  alert('Hi, Guest');
+} else if (username === 'guest') {
+  alert('Incorrect guest password');
+} else {
+  alert('Incorrect login data');
+}
+
+
